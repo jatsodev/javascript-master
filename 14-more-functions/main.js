79edc44cdd5e142b1c6185d9c5a24fd724957d77
jatso <jatorressolano@gmail.com>
2025-01-31 01:11:14 -0600

    scope

--- 14-more-functions/main.js
+++ 14-more-functions/main.js
@@ -12,10 +12,15 @@ greeting();
 //the other function executes them.
 
 let times = 0;
-setInterval(function(){
+setTimeout(function(){
     times++;
     console.log("Timeout has been executed " + times + "times.")
 },1000);
+
+// setInterval(function(){
+//     times++;
+//     console.log("Timeout has been executed " + times + "times.")
+// },1000);
 
 //setInterval(greeting, 1000); //callback 
 //setInterval(greeting("Amelia"), 1000);//no callback
@@ -23,3 +28,54 @@ setInterval(function(){
 /*setInterval(function(){//solution for a function with a parameter
     greeting("Amelia");
 }, 1000);*/
+
+// Advanced Callback features
+
+function restme(n1, n2, show, multByThree){
+    let rest = n1 - n2;
+
+    show(rest);
+    multByThree(rest);
+
+    return rest;
+}
+
+restme(20, 5, function(result){
+    console.log(result);
+}, 
+function(result){
+    console.log(result*3);
+});
+
+// variable scope
+
+let web = "cemita.com";
+
+function showWeb(){
+    console.warn(web);
+    let name = "Jatso";
+    console.log(name);
+}
+
+// console.log(name); //no scope
+console.warn(web);
+showWeb();
+
+
+//global
+if(true){
+    var sport = "my favorite sport is karate"; 
+    console.log(sport);
+
+}
+
+console.log(sport);
+
+//error
+// if(true){
+//     let sport = "my favorite sport is karate"; 
+//     console.log(sport);
+
+// }
+
+// console.log(sport);
