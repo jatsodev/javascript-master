9b0dfc6ad0dd6d0fb7c7df4be55bc0de08ca3a6a
jatso <jatorressolano@gmail.com>
2025-02-04 15:54:20 -0600

    more arrays

--- 17-advanced-arrays/main.js
+++ 17-advanced-arrays/main.js
@@ -58,3 +58,55 @@ let newPokemons = pokemons.map(pokemon => {
 });
 
 console.log(newPokemons);
+
+//Multidimensional arrays
+let categories = ["Action", "Sports", "Adventures", "Fight", "Racing"];
+let games = ["Call of duty", "FIFA", "Pokemon", "Tekken", "Forza"];
+
+let videogames = [categories, games];
+
+console.log(videogames);
+
+console.log(videogames[1][3]);
+console.log(videogames[0][2]);
+
+//Two-dimensionals arrays
+
+let table = [
+    ["Product", "Quantity", "Price"],
+    ["PC",          10,         10000],
+    ["Smartphones", 20,         10000],
+    ["Printers",    15,         5000]
+];
+
+console.table(table);
+
+console.log(table[2][0], table[2][2]);
+
+table.forEach(row => {
+    console.log(row);
+});
+
+table.forEach(row => {
+    row.forEach(element => {
+        console.log(element);
+    });
+});
+
+
+document.write("<table border = 1>");
+
+table.forEach((row, index) => {
+    document.write("<tr>");
+    row.forEach(element => {
+
+        if(index === 0){
+            document.write("<th>" + element + "</th>");
+        }else{
+            document.write("<td>" + element + "</td>");
+        }
+    });
+    document.write("</tr>");
+});
+
+document.write("</table>");
