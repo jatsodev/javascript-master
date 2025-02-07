bcf5f1c23fa9a7424e751f85498bbc6a6b26ccd0
jatso <jatorressolano@gmail.com>
2025-02-07 16:21:18 -0600

    array functions

---
+++ 18-array-functions/main.js
@@ -0,0 +1,64 @@
+//Empty array
+let emptyArray = new Array(4);
+console.log(emptyArray);
+
+//Array
+
+let gadgets = ["pc", "Laptop", "Cellphone", "Tablet"];
+console.log(gadgets);
+
+//Length of the array
+
+console.log(gadgets.length);
+
+//Add element
+gadgets.push("ps5");
+gadgets.push("Camera");
+
+console.log(gadgets);
+
+//Delete element
+
+gadgets.pop();
+gadgets.pop();
+gadgets.pop();
+
+console.log(gadgets);
+
+//Add elements to the beginning
+
+gadgets.unshift("ps4");
+gadgets.unshift("psp");
+
+console.log(gadgets);
+
+//Add elements to the end
+
+gadgets.shift();
+gadgets.shift();
+
+console.log(gadgets);
+
+//Search returns index
+
+let index = gadgets.indexOf("Laptop");
+
+console.log(index);
+
+//Search to see if element exists
+
+let exist = gadgets.includes("pc");
+
+console.log(exist);
+
+//Filter arrays
+
+gadgets.push("ps4");
+gadgets.push("ps5");
+gadgets.push("robot");
+gadgets.push("server");
+gadgets.push("smartphone");
+
+let gadgetsWithA = gadgets.filter(gadget => gadget.includes("a"));
+
+console.log(gadgetsWithA);
